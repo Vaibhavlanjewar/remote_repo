f4cf13ebcb0187b80586d11a79e4773d1af6db4c
Vaibhav Nivrutti Lanjewar <105924348+Vaibhavlanjewar@users.noreply.github.com>
2024-01-26 23:43:09 +0530

    Create Check_Christamas.java

---
+++ OneDrive/Desktop/JAVA/Check_Christamas.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		//https://www.codechef.com/problems/CHRISTGREET ,Basic program
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		if(n==25){
+		    System.out.println("CHRISTMAS");
+		}
+		else{
+		    System.out.println(" ORDINARY");
+		}
+
+	}
+}
+// Vnbl
